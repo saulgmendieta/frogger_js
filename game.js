f6ce7d66835a7ee80905ae07b4bcc02a385ad159
Greg <souldrone91@gmail.com>
2019-05-28 11:06:57 -0500

    Win condition

--- game.js
+++ game.js
@@ -67,13 +67,56 @@ window.addEventListener("load",function() {
 
   var frog = {
     x: 230,
-    y: 470, //470
+    y: 470,
     w: 50,
     h: 50
   };
   var player;
   player = new Image();
   player.src = 'img/frog.png';
+
+  var wins = [
+    {
+      x: 8,
+      y: 10,
+      show: false,
+      w: 50,
+      h: 50
+    },
+    {
+      x: 119,
+      y: 10,
+      show: false,
+      w: 180,
+      h: 60
+    },
+    {
+      x: 230,
+      y: 10,
+      show: false,
+      w: 180,
+      h: 60
+    },
+    {
+      x: 341,
+      y: 10,
+      show: false,
+      w: 180,
+      h: 60
+    },
+    {
+      x: 452,
+      y: 10,
+      show: false,
+      w: 180,
+      h: 60
+    }
+  ];
+  var claimed = [false,false,false,false,false];
+  var goals;
+  goals = new Image();
+  goals.src = 'img/frog2.png';
+
   // Game refresh
   var step = function() {
     ctx.clearRect(0,0,GAME_WIDTH,GAME_HEIGHT);
@@ -85,6 +128,12 @@ window.addEventListener("load",function() {
       ctx.drawImage(wood_img, element.x, element.y);
     });
     ctx.drawImage(player, frog.x, frog.y);
+    wins.forEach(function(element, index){
+      if(element.show==true){
+        ctx.drawImage(goals, element.x, element.y);
+      }
+    });
+    win_condition();
     window.requestAnimationFrame(step);
   };
 
@@ -100,7 +149,13 @@ window.addEventListener("load",function() {
           else if(frog.y>65){
             frog.y -= 55;
           }
-          else if(frog.y>10 && (frog.x==8 || frog.x==119 || frog.x==230 || frog.x==341 || frog.x==452)){
+          else if(frog.y>10 &&
+                  ((frog.x==8 && wins[0].show!=true) ||
+                   (frog.x==119 && wins[1].show!=true) ||
+                   (frog.x==230 && wins[2].show!=true) ||
+                   (frog.x==341 && wins[3].show!=true) ||
+                   (frog.x==452 && wins[4].show!=true)
+                  )){
             frog.y -= 55;
           }
         }
@@ -123,15 +178,45 @@ window.addEventListener("load",function() {
           }
         }
         console.log('X: ', frog.x, ' , Y: ', frog.y);
+        if(frog.y==10){
+          if(frog.x==8){
+            wins[0].show = true;
+          }
+          else if(frog.x==119){
+            wins[1].show = true;
+          }
+          else if(frog.x==230){
+            wins[2].show = true;
+          }
+          else if(frog.x==341){
+            wins[3].show = true;
+          }
+          else if(frog.x==452){
+            wins[4].show = true;
+          }
+          frog.x = 230;
+          frog.y = 470;
+        }
         once = 1;
       }
     }
   }, false);
 
   window.addEventListener('keyup', function (e) {
-    //console.log("Stop");
     once = 0;
   }, false);
+
+
+
+  var win_condition = function(e) {
+    if(wins[0].show == true && wins[1].show == true &&
+       wins[2].show == true && wins[3].show == true &&
+       wins[4].show == true){
+         alert('You\'ve won!');
+         window.location = "";
+       }
+    console.log();
+  }
 
   step();
 
